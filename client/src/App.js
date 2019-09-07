6568fec156b2671a7f3a885e73b06cd5dd1e9ece
CJ Vaughn <cjvaughn@rocketmail.com>
2019-09-07 13:11:34 -0700

    Set up rough draft of layout for the blog and current stats of the warlock.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
-      <p>Placeholder</p> 
+      <Row>
+        <Col xs={8} md={9} >
+          <Card bg='danger' text='dark' >
+            <Card.Body>
+              <Card.Title>Blog</Card.Title>
+              <Card.Text>
+                Placeholder
+             </Card.Text>
+            </Card.Body>
+          </Card></Col>
+        <Col xs={4} md={3}>
+          <Card bg='dark' text='warning' >
+            <Card.Body>
+              <Card.Title>Current Stats</Card.Title>
+              <Card.Text>
+                Placeholder
+             </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
     </div>
   );
 }
